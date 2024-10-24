e1f184ce4b978c6d6a4146bd8a3523b25e01d3ee
Marco Antonio <159474548+Markyneos@users.noreply.github.com>
2024-10-24 09:45:03 -0300

    Update questao7.py

--- questao7.py
+++ questao7.py
@@ -9,17 +9,17 @@ e) Nenhum dos códigos produz o mesmo resultado.
 
 #Código A:
 lista = [x for x in range(5)]
-print(lista)
+print(lista) #[0, 1, 2, 3, 4]
 #Código B:
 lista = list(range(5))
-print(lista)
+print(lista) #[0, 1, 2, 3, 4]
 #Código C:
 lista = []
 i = 0
 while i < 5:
     lista.append(i)
     i += 1
-print(lista)
+print(lista) #[0, 1, 2, 3, 4]
 
 #Resposta: Letra d.
 #Relatório: Marquei em minha prova a letra d.
